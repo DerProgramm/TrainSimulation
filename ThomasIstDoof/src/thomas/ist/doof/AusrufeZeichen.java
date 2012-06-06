f679e9a95ab433cd4fe0c4779521b63d100446e7
DerProgramm <wuensche.e@googlemail.com>
2012-06-06 10:26:49 +0300

    - GUI fertig und Blockierung von Strecken implementiert

--- ThomasIstDoof/src/thomas/ist/doof/AusrufeZeichen.java
+++ ThomasIstDoof/src/thomas/ist/doof/AusrufeZeichen.java
@@ -7,6 +7,7 @@ public class AusrufeZeichen {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Thomas ist doof!");
+		System.out.println("Man klaut nicht einfach Code!");
 	}
 
 }
