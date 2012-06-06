d21664d07eb0c1b45f9c23983dcb6682a2b5134d
Eric <Eric@Schlepptop>
2012-06-06 08:47:01 +0200

    - GUI fertig und Blockierung von Strecken implementiert

---
+++ ThomasIstDoof/src/thomas/ist/doof/AusrufeZeichen.java
@@ -0,0 +1,12 @@
+package thomas.ist.doof;
+
+public class AusrufeZeichen {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Thomas ist doof!");
+	}
+
+}
